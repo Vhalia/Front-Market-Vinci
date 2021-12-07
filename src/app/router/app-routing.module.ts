645e5d9cf2356159fddfcf2f0e6ba2f20aa348d1
Adrien de Decker <adrien.dedecker@student.vinci.be>
2021-12-07 16:36:51 +0100

    add productCart component

--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from '../Components/home/home.component'
+import { HomeComponent } from '../Components/home/home.component';
 import { SearchComponent } from '../Components/search/search.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path:'search', component: SearchComponent}
+  { path: '', component: HomeComponent },
+  { path: 'recherche', component: SearchComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
