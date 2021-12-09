b644c5cbe6e4e9176538104851a24ce887c55933
Jehan de Foy <jehan.defoy@student.vinci.be>
2021-12-09 12:05:22 +0100

    adding profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,6 @@ import { HomeComponent } from './Components/home/home.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { SearchComponent } from './Components/search/search.component';
 import { ProfileComponent } from './Components/profile/profile.component';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzListModule } from 'ng-zorro-antd/list';
 import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddProductComponent } from './Components/add-product/add-product.component'; 
@@ -31,9 +28,6 @@ import { AddProductComponent } from './Components/add-product/add-product.compon
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NzButtonModule,
-    NzGridModule,
-    NzListModule,
     DemoNgZorroAntdModule,
     BrowserAnimationsModule
   ],
--- src/app/ng-zorro-antd.module.ts
+++ src/app/ng-zorro-antd.module.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzListModule } from 'ng-zorro-antd/list';
 
 @NgModule({
   exports: [
     NzButtonModule,
-    NzDropDownModule
+    NzDropDownModule,
+    NzButtonModule,
+    NzGridModule,
+    NzListModule
   ]
 })
 export class DemoNgZorroAntdModule {
