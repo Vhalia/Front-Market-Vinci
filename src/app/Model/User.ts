ecb82d1981cd00dac6e4f21417e8b72ea3ae9dc6
Adrien de Decker <adriendedecker@yahoo.fr>
2021-12-14 11:13:13 +0100

    add error + modif User model

--- src/app/Model/User.ts
+++ src/app/Model/User.ts
@@ -1,13 +1,20 @@
-import { Rating } from "./Rating";
+import { Product } from './Product';
+import { Rating } from './Rating';
 
 export interface User {
-    id : string;
-    name : string;
-    surname : string;
-    mail : string;
-    campus : string;
-    password : string;
-    isBanned : boolean;
-    isAdmin : boolean;
-    ratings: Rating[];
+  id: string;
+  name: string;
+  surname: string;
+  mail: string;
+  campus: string;
+  password: string;
+  isBanned: boolean;
+  isAdmin: boolean;
+  ratings: Rating[];
+  image: string;
+  favTypes: undefined;
+  sold: string[];
+  bought: string[];
+  favProducts: string[];
+  //   badges:
 }
